665dc9f98b5c7ede5f91deb288138d39a476706d
StarPlatinum-17 <61497306+StarPlatinum-17@users.noreply.github.com>
2020-03-18 23:31:55 +0800

    use form in all views, back to working state

--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -8,7 +8,7 @@ class LayoutAndStylingTest(FunctionalTest):
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
         # She notices the input box is nicely centered
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_text')
         self.assertAlmostEqual(
         inputbox.location['x'] + inputbox.size['width'] / 2,
         512,
