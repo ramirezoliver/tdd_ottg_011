df14e5c979e9cc68b93f46b459ec6aa9fc12d987
StarPlatinum-17 <61497306+StarPlatinum-17@users.noreply.github.com>
2020-03-19 00:16:19 +0800

    Revert "latest_head"
    This reverts commit ed464366977efce4ff14410ffc3cda63c6147816.

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -9,7 +9,7 @@ class ItemValidationTest(FunctionalTest):
         # Edith goes to the home page and accidentally tries to submit
         # an empty list item. She hits Enter on the empty input box
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys(Keys.ENTER)
     
         # The browser intercepts the request, and does not load the
         # list page
@@ -17,7 +17,6 @@ class ItemValidationTest(FunctionalTest):
             lambda: self.browser.find_element_by_css_selector(
                 '#id_text:invalid' 
         ))
-<<<<<<< HEAD
         
         # She starts typing some text for the new item and the error disappears
         self.get_item_input_box().send_keys('Buy milk')    
@@ -28,17 +27,11 @@ class ItemValidationTest(FunctionalTest):
          
         # And she can submit it successfully
         self.get_item_input_box().send_keys(Keys.ENTER)   
-=======
-
-        # She tries again with some text for the item, which now work
-        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')    
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)    
->>>>>>> parent of fb533f7... rename all item input ids and names. still broken"
         self.wait_for_row_in_list_table('1: Buy milk')
 
         
         # Perversely, she now decides to submit a second blank list item    
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys(Keys.ENTER)
         
         # Again, the browser will not comply 
         self.wait_for_row_in_list_table('1: Buy milk')
@@ -49,16 +42,11 @@ class ItemValidationTest(FunctionalTest):
 
         
         # And she can correct it by filling some text in    
-<<<<<<< HEAD
         self.get_item_input_box().send_keys('Make tea')   
         self.wait_for(
             lambda: self.browser.find_element_by_css_selector(
                 '#id_text:valid' 
         ))
         self.get_item_input_box().send_keys(Keys.ENTER)    
-=======
-        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')    
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)    
->>>>>>> parent of fb533f7... rename all item input ids and names. still broken"
         self.wait_for_row_in_list_table('1: Buy milk')    
         self.wait_for_row_in_list_table('2: Make tea')
--- lists/views.py
+++ lists/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import redirect, render
 from django.core.exceptions import ValidationError
 from lists.models import Item, List
+from lists.forms import ItemForm
 
 def home_page(request):
     return render(request, 'home.html', {'form': ItemForm()})
 
 def new_list(request):
-<<<<<<< HEAD
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List.objects.create()
@@ -14,32 +14,14 @@ def new_list(request):
         return redirect(list_)
     else:
         return render(request, 'home.html', {"form": form})
-=======
-    list_ = List.objects.create()
-    item = Item.objects.create(text=request.POST['item_text'], list=list_)
-    try:
-        item.full_clean()
-    except ValidationError:
-        list_.delete()
-        error = "You can't have an empty list item"
-        return render(request, 'home.html',{"error":error})
-    return redirect(list_)
->>>>>>> parent of fb533f7... rename all item input ids and names. still broken"
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
     form = ItemForm()
     if request.method == 'POST':
-<<<<<<< HEAD
         form = ItemForm(data=request.POST)
         if form.is_valid():
             Item.objects.create(text=request.POST['text'], list = list_)
-=======
-        try:
-            item = Item(text=request.POST['item_text'],list=list_)
-            item.full_clean()   
-            item.save()            
->>>>>>> parent of fb533f7... rename all item input ids and names. still broken"
             return redirect(list_)
     return render(request, 'list.html', {
         'list': list_, "form": form
