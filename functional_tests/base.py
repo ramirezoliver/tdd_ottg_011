e8de30a4d7cba3addc559f243d38759bf1864485
StarPlatinum-17 <ramirezoliver.g@gmail.com>
2020-03-28 23:55:58 +0800

    serverside_patch for test

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,11 +1,12 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
+from .server_tools import reset_database
 import time
 import os
 
-MAX_WAIT = 5
 
+MAX_WAIT = 5
 
 def wait(fn):
     def modified_fn(*args, **kwargs):
@@ -20,17 +21,18 @@ def wait(fn):
 
     return modified_fn
 
+
 class FunctionalTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
+        self.staging_server = os.environ.get('STAGING_SERVER')
+        if self.staging_server:
+            self.live_server_url = 'http://' + self.staging_server
+            reset_database(self.staging_server)
 
     def tearDown(self):
         self.browser.quit()
-
 
     @wait
     def wait_for(self, fn):
